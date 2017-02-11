8ff7ce7d612d99678451f471b48e3d27b7a53936
Alfiya <brskasimova@gmail.com>
2017-02-11 22:22:58 +0300

    Создан новый пакет distance с 2 классами: Poin, Distance

---
+++ sandbox/src/main/java/ru/stqa/pft/distance/Distance.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.distance;
+
+/**
+ * Created by Альфия on 11.02.2017.
+ */
+public class Distance {
+    public static void main (String[] args) {
+
+        Point p1 = new Point(1,10);
+        Point p2 = new Point(10,10);
+
+        System.out.println("Расстояние между точками " + "(" + p1.x + "," + p1.y + ")" + " и " + "(" + p2.x + "," + p2.y + ")" + " = " + p1.Distance(p2));
+
+    }
+}
