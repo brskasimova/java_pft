ab7dea8013a09a5da858febc2ef7995268fc116d
Alfiya <brskasimova@gmail.com>
2017-02-18 16:36:09 +0300

    Класс ApplicationManager разбит на вспомогательные классы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -7,10 +7,10 @@ public class ContactCreationTests extends TestBase {
 
     @Test
     public void testContactCreation() {
-        app.initContactCreation();
-        app.fillContactForm(new ContactData("testfirstname2", "testlastname2", "testaddress2", "testhomephone2", "testemail2"));
-        app.submitContactCreation();
-        app.dotoHomePage();
+        app.getContactHelper().initContactCreation();
+        app.getContactHelper().fillContactForm(new ContactData("testfirstname2", "testlastname2", "testaddress2", "testhomephone2", "testemail2"));
+        app.getContactHelper().submitContactCreation();
+        app.getContactHelper().returntoHomePage();
     }
 
 }
