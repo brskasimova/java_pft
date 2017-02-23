9f141d0e74777660ef2af9a450e75d43e0fd15a6
Alfiya <brskasimova@gmail.com>
2017-02-23 22:18:02 +0300

    Добавлено условие проверки текстового поля на null

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -9,7 +9,7 @@ public class GroupCreationTests extends TestBase {
     public void testGroupCreation() {
         app.getNavigationHelper().gotoGroupPage();
         app.getGroupHelper().initGroupCreation();
-        app.getGroupHelper().fillGroupForm(new GroupData("testname1", "testheader", "testfooter"));
+        app.getGroupHelper().fillGroupForm(new GroupData("testname1", null, null));
         app.getGroupHelper().submitGroupCreation();
         app.getGroupHelper().returnToGroupPage();
     }
