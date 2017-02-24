0ccc9fb1ba678f8f081fc14ef00a69eb992b554e
Alfiya <brskasimova@gmail.com>
2017-02-24 10:41:20 +0300

    Оптимизированы переходы между страницами

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.Objects;
+
 public class NavigationHelper extends HelperBase {
 
     public NavigationHelper(WebDriver wd) {
@@ -11,9 +13,20 @@ public class NavigationHelper extends HelperBase {
     }
 
     public void gotoGroupPage() {
-        click(By.linkText("groups"));
+        if (isElementPresent(By.tagName("h1"))
+                && Objects.equals(wd.findElement(By.tagName("h1")).getText(), "Groups")
+                && isElementPresent(By.name("new"))) {
+            return;
+        } else {
+            click(By.linkText("groups"));
+        }
     }
 
-    public void gotoHomePage() { click(By.linkText("home")); }
-
+    public void gotoHomePage() {
+        if (isElementPresent(By.id("maintable"))) {
+            return;
+        } else {
+            click(By.linkText("home"));
+        }
+    }
 }
