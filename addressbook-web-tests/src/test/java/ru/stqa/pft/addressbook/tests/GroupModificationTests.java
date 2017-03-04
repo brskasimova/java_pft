7139500a97925a797c6268d47313f497b52a2282
Alfiya <brskasimova@gmail.com>
2017-03-04 10:53:26 +0300

    Добавлен выбор по порядковому номеру элемента для тестов групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -13,7 +13,7 @@ public class GroupModificationTests extends TestBase {
         if (! app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("testname1", null, null));
         }
-        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().selectGroup(before - 1);
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("modifyname", "modifyheader", "modifyfooter"));
         app.getGroupHelper().submitGroupModification();
