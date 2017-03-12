fd4107c5dc8fd3f45ce87aa44f3237c3327d65f1
Alfiya <brskasimova@gmail.com>
2017-03-12 10:11:10 +0300

    Реализованы тесты проверки почтового адреса и адресов электронной почты

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddressTests.java
@@ -0,0 +1,30 @@
+package ru.stqa.pft.addressbook.tests;
+
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ContactAddressTests extends TestBase {
+
+    @BeforeMethod
+    public void ensurePreconditions() {
+        app.goTo().homePage();
+        if (app.contact().all().size() == 0) {
+            app.contact().create(new ContactData().withFirstname("contactname").withLastname("contactlastname"));
+        }
+    }
+
+    @Test
+    public void testContactAddess() {
+        app.goTo().homePage();
+        ContactData contact = app.contact().all().iterator().next();
+        ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+
+        assertThat(contact.getAddress(), equalTo(contactInfoFromEditForm.getAddress()));
+    }
+
+}
