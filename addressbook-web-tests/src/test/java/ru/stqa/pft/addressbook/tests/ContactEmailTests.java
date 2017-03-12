3fc3e763b59c50227834cb4e2e871e50641cb5f0
Alfiya <brskasimova@gmail.com>
2017-03-12 10:12:43 +0300

    Реализованы тесты проверки адресов и email

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactEmailTests.java
@@ -0,0 +1,46 @@
+package ru.stqa.pft.addressbook.tests;
+
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ContactEmailTests extends TestBase{
+
+    @BeforeMethod
+    public void ensurePreconditions() {
+        app.goTo().homePage();
+        if (app.contact().all().size() == 0) {
+            app.contact().create(new ContactData().withFirstname("contactname").withLastname("contactlastname"));
+        }
+    }
+
+    @Test
+    public void testContactPhones() {
+        app.goTo().homePage();
+        ContactData contact = app.contact().all().iterator().next();
+        ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+
+        assertThat(contact.getAllEmails(), equalTo(mergeEmails(contactInfoFromEditForm)));
+
+    }
+
+    private String mergeEmails(ContactData contact) {
+        return Arrays.asList(contact.getEmail(), contact.getEmail2(), contact.getEmail3())
+                .stream().filter((e) -> !e.equals(""))
+                .map(ContactEmailTests::cleaned)
+                .collect(Collectors.joining("\n"));
+    }
+
+    public static String cleaned(String email) {
+        return email.replaceAll("\\s", "");
+
+    }
+
+}
