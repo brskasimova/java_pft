a557114ee10adf2f50e381a4d960518a5f177a8e
Alfiya <brskasimova@gmail.com>
2017-04-01 09:09:44 +0300

    Внесены изменения в связи с заменой версии Mantis

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
@@ -34,7 +34,7 @@ public class HttpSession {
         post.setEntity(new UrlEncodedFormEntity(params));
         CloseableHttpResponse response = httpclient.execute(post);
         String body = geTextFrom(response);
-        return body.contains(String.format("<span class=\"user-info\">%s</span>", username));
+        return body.contains(String.format("<span class=\"italic\">%s</span>", username));
     }
 
     private String geTextFrom(CloseableHttpResponse response) throws IOException {
@@ -46,9 +46,9 @@ public class HttpSession {
     }
 
     public boolean isLoggedInAs(String username) throws IOException {
-        HttpGet get = new HttpGet(app.getProperty("web.baseUrl") + "/account_page.php");
+        HttpGet get = new HttpGet(app.getProperty("web.baseUrl") + "/index.php");
         CloseableHttpResponse response = httpclient.execute(get);
         String body = geTextFrom(response);
-        return body.contains(String.format("<span class=\"user-info\">%s</span>", username));
+        return body.contains(String.format("<span class=\"italic\">%s</span>", username));
     }
 }
