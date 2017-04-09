7dc55279f687da976769b1e23fb0c0daf3e9d298
Alfiya <brskasimova@gmail.com>
2017-04-09 15:31:00 +0300

    Реализована интеграция тестов с MantisBT, проверка Resolution

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/SoapHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/SoapHelper.java
@@ -23,7 +23,7 @@ public class SoapHelper {
 
     public Set<Project> getProjects() throws MalformedURLException, ServiceException, RemoteException {
         MantisConnectPortType mc = getMantisConnect();
-        ProjectData[] projects = mc.mc_projects_get_user_accessible("administrator", "root");
+        ProjectData[] projects = mc.mc_projects_get_user_accessible(app.getProperty("web.adminLogin"), app.getProperty("web.adminPass"));
         return Arrays.asList(projects).stream()
                 .map((p) -> new Project().withId(p.getId().intValue()).withName(p.getName()))
                 .collect(Collectors.toSet());
@@ -31,22 +31,34 @@ public class SoapHelper {
 
     private MantisConnectPortType getMantisConnect() throws ServiceException, MalformedURLException {
         return new MantisConnectLocator()
-                .getMantisConnectPort(new URL("http://localhost/mantisbt-1.2.19/api/soap/mantisconnect.php"));
+                .getMantisConnectPort(new URL(app.getProperty("soap.url")));
     }
 
     public Issue addIssue(Issue issue) throws MalformedURLException, ServiceException, RemoteException {
         MantisConnectPortType mc = getMantisConnect();
-        String[] categories = mc.mc_project_get_categories("administrator", "root", BigInteger.valueOf(issue.getProject().getId()));
+        String[] categories = mc.mc_project_get_categories(app.getProperty("web.adminLogin"), app.getProperty("web.adminPass"), BigInteger.valueOf(issue.getProject().getId()));
         IssueData issueData = new IssueData();
         issueData.setSummary(issue.getSummary());
         issueData.setDescription(issue.getDescription());
         issueData.setProject(new ObjectRef(BigInteger.valueOf(issue.getProject().getId()), issue.getProject().getName()));
         issueData.setCategory(categories[0]);
-        BigInteger issueId = mc.mc_issue_add("administrator", "root", issueData);
-        IssueData createdIssueData = mc.mc_issue_get("administrator", "root", issueId);
+        BigInteger issueId = mc.mc_issue_add(app.getProperty("web.adminLogin"), app.getProperty("web.adminPass"), issueData);
+        IssueData createdIssueData = mc.mc_issue_get(app.getProperty("web.adminLogin"), app.getProperty("web.adminPass"), issueId);
         return new Issue().withId(createdIssueData.getId().intValue())
                 .withSummary(createdIssueData.getSummary()).withDescription(createdIssueData.getDescription())
                 .withProject(new Project().withId(createdIssueData.getProject().getId().intValue())
                         .withName(createdIssueData.getProject().getName()));
+    }
+
+    public Issue getIssueById(int issueId) throws MalformedURLException, ServiceException, RemoteException {
+        MantisConnectPortType mantisConnectPort = getMantisConnect();
+        IssueData issue = mantisConnectPort.mc_issue_get(app.getProperty("web.adminLogin"), app.getProperty("web.adminPass"), BigInteger.valueOf(issueId));
+        ObjectRef status = issue.getStatus();
+        status.getName();
+        return  new Issue().withId(issue.getId().intValue()).withSummary(issue.getSummary()).
+                withDescription(issue.getDescription()).withStatus(issue.getStatus().getName()).
+                withResolution(issue.getResolution().getName()).withProject(new Project().
+                withId(issue.getProject().getId().intValue()).
+                withName(issue.getProject().getName()));
     }
 }
--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/SoapTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/SoapTests.java
@@ -1,6 +1,6 @@
 package ru.stqa.pft.mantis.tests;
 
-import org.testng.Assert;
+import org.testng.SkipException;
 import org.testng.annotations.Test;
 import ru.stqa.pft.mantis.model.Issue;
 import ru.stqa.pft.mantis.model.Project;
@@ -16,6 +16,11 @@ public class SoapTests extends TestBase{
 
     @Test
     public void testGetProjects() throws MalformedURLException, ServiceException, RemoteException {
+        try {
+            skipIfNotFixed(2);
+        } catch (SkipException e) {
+            e.printStackTrace();
+        }
         Set<Project> projects = app.soap().getProjects();
         System.out.println(projects.size());
         for (Project project : projects) {
@@ -26,8 +31,8 @@ public class SoapTests extends TestBase{
     @Test
     public void testCreateIssue() throws MalformedURLException, ServiceException, RemoteException {
         Set<Project> projects = app.soap().getProjects();
-        Issue issue = new Issue().withSummary("Test issue")
-                .withDescription("Test issue description").withProject(projects.iterator().next());
+        Issue issue = new Issue().withSummary("Test issue4")
+                .withDescription("Test issue description4").withProject(projects.iterator().next());
         Issue created = app.soap().addIssue(issue);
         assertEquals(issue.getSummary(), created.getSummary());
     }
